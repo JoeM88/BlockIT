7cb251aa1b46076a6fca16b9d6036209e073058b
Joseph Molina <josephmolina3388@gmail.com>
2018-12-10 10:40:33 -0800

    Initial commit

---
+++ app/src/main/java/e/josephmolina/blockit/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package e.josephmolina.blockit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
